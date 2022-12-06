9b6e9bf5d11eb988a477876966383fb1cc8f8066
Vale <oirelav95@gmail.com>
2022-12-06 17:18:56 +0100

    stampo le email

--- js/myscript.js
+++ js/myscript.js
@@ -6,3 +6,33 @@
 // (chiaramente usando Vue, etc.,)
 // Bonus
 // Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            email: 10,
+            emails: [],
+        }
+    },
+    methods: {
+
+        printEmails() {
+            for (let i = 0; i < this.email; i++) {
+
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) => {
+                    
+                    this.emails.push(result.data.response);
+    
+                });
+    
+            }
+        }
+    },
+    mounted() {
+
+        this.printEmails();
+
+    }
+}).mount('#app')
